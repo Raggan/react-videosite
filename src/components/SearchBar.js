4f3cc4cefb017ccfcb031837f21819348c25c3a8
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-13 12:24:37 +0200

    Searchbar initiate

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 
 class SearchBar extends Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="ui raised segment">
+        <form>
+          <div className="ui fluid input">
+            <input type="text" placeholder="Search for Videos..." />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
